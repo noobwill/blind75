6cfe797ca911b6999e5a41b27a8beb1d7c9f608c
wshin <wl751242@dal.ca>
2024-02-19 08:38:04 -0400

    Adding quadratic time solution to Contains Duplicate question

---
+++ 3 - Contains Duplicate/Solution1.java
@@ -0,0 +1,16 @@
+// time complexity of O(n^2)
+public Solution1{
+  public boolean containsDuplicate(int[] nums){
+    int n = nums.length;
+
+    for (int i = 0; i < n; i++){
+      for (int j = 0; j < i; j++){
+        if (nums[j] == nums[i]){
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+}
