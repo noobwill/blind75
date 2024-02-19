dc0245d76e6e7a93863ddcec65a9d108e85c0bcc
wshin <wl751242@dal.ca>
2024-02-19 09:38:18 -0400

    Hash Map approach

---
+++ 3 - Contains Duplicate/Solution4.java
@@ -0,0 +1,20 @@
+import java.util.Map;
+import java.util.HashMap;
+
+public Solution4{
+  public boolean containsDuplicate(int[] nums){
+    int n = nums.length;
+
+    Map<Integer, Integer> map = new HashMap<>();
+
+    for (int num : nums){
+      if (map.containsKey(num) && map.get(num) >= 1){
+        return true;
+      }
+
+      map.put(num, map.getOrDefault(num, 0) + 1);
+    }
+
+    return false;
+  }
+}
