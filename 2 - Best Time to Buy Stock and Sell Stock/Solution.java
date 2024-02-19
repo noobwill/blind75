22027f3420f2a4954daa343a24dccb153b6980c4
wshin <wl751242@dal.ca>
2024-02-19 10:47:32 -0400

    Added comments

--- 2 - Best Time to Buy Stock and Sell Stock/Solution.java
+++ 2 - Best Time to Buy Stock and Sell Stock/Solution.java
@@ -1,17 +1,24 @@
 public class Solution{
   public int findMaxProfit(int[] prices){
+    // left is buy price
     int left = 0;
+    // right is sell price
     int right = 1;
+    // zero profit at the beginning
     int max_profit = 0;
 
+    // iterate while the right pointer has not passed past the end of the array, i.e. until out of bounds
     while (right < prices.length){
+      // if transaction is profitable, compute profit and compare against max profit so far
       if (prices[left] < prices[right]){
         int curr_profit = prices[right] - prices[left];
         max_profit = Math.max(curr_profit, max_profit);
       }
+      // if transaction is not profitable, update left pointer to be at new minimum
       else{
         left = right;
       }
+      // update right pointer regardless
       right++;
     }
 
